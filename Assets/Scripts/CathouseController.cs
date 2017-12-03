58a48318b9543dc80bf1405e3d2cf79299a9785e
Andreas Ragen Ayal <andreas.r.ayal@gmail.com>
2017-12-03 18:36:58 +0200

    Kittens vanish when they enter the Cathouse

---
+++ Assets/Scripts/CathouseController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CathouseController : MonoBehaviour
+{
+
+	[HideInInspector]public int caughtKittens;
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.gameObject.tag == "Kitten")
+		{
+			caughtKittens++;
+			Destroy (other.gameObject);
+		}
+	}
+}
