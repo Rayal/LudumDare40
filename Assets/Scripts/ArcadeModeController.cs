512768b71b456dc116079e95e8d0894ba52239b0
Andreas Ragen Ayal <andreas.r.ayal@gmail.com>
2017-12-04 12:11:43 +0200

    Longer catch time: 3s. Build

--- Assets/Scripts/ArcadeModeController.cs
+++ Assets/Scripts/ArcadeModeController.cs
@@ -1,10 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ArcadeModeController : MonoBehaviour
 {
-
 	public float levelLeftBorder;
 	public float levelRightBorder;
 	public float levelHeight;
